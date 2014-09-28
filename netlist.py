05f4d5349f6529fa89e78d9cd178aa426ddf4653
Christoph Müller <christoph@christophmueller.org>
2014-09-28 23:18:38 +0200

    netlist class extended (wip)

--- netlist.py
+++ netlist.py
@@ -8,9 +8,16 @@ class netlist_element():
 class subcircuit(netlist_element):
     def __init__(self, name, nets, instances):
         self.name = name;
-        self.nets = nets;
+
+        # dictionarry of net names,
+        # key is net name, value is net object
+        self.nets = {} #= nets;
+        for n in nets:
+            self.nets[n] = net(n)
+        
         self.instances = instances;
         netlist_element.__init__(self,'subcircuit')
+
     def __str__(self):
         insts = {}
         for i in self.instances:
@@ -31,4 +38,35 @@ class net:
     def __init__(self, name):
         self.name = name
         self.nettype = 'standard'
- 
+        self.nodes = set()
+
+    def connect(self, pin):
+        self.nodes.add(pin)
+
+class pin:
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+        self.net = False
+
+    def connect(self,net):
+        if not self.net:
+            self.net = net
+        
+class mosfet:
+    def __init__(self,name):
+        self.name = name
+        self.drain = pin('d',self)
+        self.gate = pin('g',self)
+        self.source = pin('s',self)
+        self.bulk = pin('s',self)
+
+class nmos(mosfet):
+    def __init__(self,name):
+        self.type = 'n'
+        mosfet.__init__(self, name)
+
+class pmos(mosfet):
+    def __init__(self,name):
+        self.type = 'p'
+        mosfet.__init__(self, name)
